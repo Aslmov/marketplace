0fda3e9c9e3bca20bee2b49731c1d5bea7484c74
SELORM  FABRICE <selormfabrice@gmail.com>
2023-09-06 13:47:16 +0000

    items removed from gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 // import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 // import 'package:flutter/foundation.dart';
 import 'dart:ui';
-
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
