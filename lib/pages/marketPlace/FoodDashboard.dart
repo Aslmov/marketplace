387befb5057ec09dd11f740d9b6ce45fca05d027
morguen.dev <morguen.dev@gmail.com>
2023-09-28 17:56:34 +0200

    last change

--- lib/pages/marketPlace/FoodDashboard.dart
+++ lib/pages/marketPlace/FoodDashboard.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:material_floating_search_bar_2/material_floating_search_bar_2.dart';
 import 'package:nb_utils/nb_utils.dart';
 import 'package:tagxisuperuser/styles/styles.dart';
 import '../../functions/functions.dart';
@@ -81,6 +82,56 @@ class FoodDashboardState extends State<FoodDashboard> {
       );
     }
 
+    Widget buildFloatingSearchBar() {
+      final isPortrait =
+          MediaQuery.of(context).orientation == Orientation.portrait;
+
+      return FloatingSearchBar(
+        hint: 'Search...',
+        scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+        transitionDuration: const Duration(milliseconds: 800),
+        transitionCurve: Curves.easeInOut,
+        physics: const BouncingScrollPhysics(),
+        axisAlignment: isPortrait ? 0.0 : -1.0,
+        openAxisAlignment: 0.0,
+        width: isPortrait ? 600 : 500,
+        debounceDelay: const Duration(milliseconds: 500),
+        onQueryChanged: (query) {
+          // Call your model, bloc, controller here.
+        },
+        // Specify a custom transition to be used for
+        // animating between opened and closed stated.
+        transition: CircularFloatingSearchBarTransition(),
+        actions: [
+          FloatingSearchBarAction(
+            showIfOpened: false,
+            child: CircularButton(
+              icon: const Icon(Icons.place),
+              onPressed: () {},
+            ),
+          ),
+          FloatingSearchBarAction.searchToClear(
+            showIfClosed: false,
+          ),
+        ],
+        builder: (context, transition) {
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Material(
+              color: Colors.white,
+              elevation: 4.0,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: Colors.accents.map((color) {
+                  return Container(height: 112, color: color);
+                }).toList(),
+              ),
+            ),
+          );
+        },
+      );
+    }
+
     return Scaffold(
       key: _scaffoldKey,
       body: SafeArea(
@@ -117,7 +168,6 @@ class FoodDashboardState extends State<FoodDashboard> {
                         children: <Widget>[
                           //mAddress(context),
                           SizedBox(height: 16),
-                          search(context),
                           SizedBox(height: 16),
                           //AdsBox(revere: false),
                           CategorieBox(
@@ -614,7 +664,6 @@ class FoodSideMenuState extends State<FoodSideMenu> {
                                 )
                               ],
                             ),
-                         
                           ],
                         ),
                       ),
